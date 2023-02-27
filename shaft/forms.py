a5f0ec283c44e40fad5ca74f9b116a5595ed9f2c
DrieHarry <andreharry89@gmail.com>
2023-02-27 21:49:25 +0800

    Hapus torsi ganti daya dan kecepatan putar; tambh kekuatan lelah aktual dari tegangan tarik

--- shaft/forms.py
+++ shaft/forms.py
@@ -5,14 +5,12 @@ from .models import *
 class MyForm(forms.Form):
     N = forms.FloatField(label="Faktor Keamanan ",
                             widget=forms.NumberInput(attrs={'value': '2.0', 'min': '0'}))
-    T = forms.FloatField(label="Momen Puntir / Torsi ",
-                            widget=forms.NumberInput(attrs={'placeholder': 'N-mm', 'min': '0'}))
+    P = forms.FloatField(label="Daya ",
+                            widget=forms.NumberInput(attrs={'placeholder': 'kW', 'min': '0'}))
+    n = forms.FloatField(label="Kecepatan Putar ",
+                            widget=forms.NumberInput(attrs={'placeholder': 'rpm', 'min': '0'}))
     Material = forms.ModelChoiceField(
                             label = "Pilih Bahan :", queryset = Materials.objects.all())
-    Sy = forms.FloatField(label="Tegangan Luluh Bahan ",
-                            widget=forms.NumberInput(attrs={'placeholder': 'MPa', 'min': '0'}))
-    Sn = forms.FloatField(label="Tegangan / Kekuatan Lelah Aktual ",
-                            widget=forms.NumberInput(attrs={'placeholder': 'MPa', 'min': '0'}))
     Ft = forms.FloatField(label="Gaya Tangensial Pada Elemen (Ft) ",
                             widget=forms.NumberInput(attrs={'placeholder': 'N', 'min': '0'}))
     Fr = forms.FloatField(label="Gaya Radial Pada Elemen (Fr) ",
--- shaft/views.py
+++ shaft/views.py
@@ -1,40 +1,45 @@
 from django.shortcuts import render
+
+
 from .forms import *
 from .models import *
 
 
 import math
+import numpy as np
+import matplotlib.pyplot as plt
  
 
 # Create your views here.
 def input(request):
-    materials = Materials.objects.all()
     return render(request, "shaft/input.html", {
         "form": MyForm(),
-        "materials" : materials
     })
 
 def output(request):
 
     N = float(request.POST['N']) #Faktor Keamanan
-    T = float(request.POST['T']) #Torsi
-    Syy = float(request.POST['Sy']) #Tegangan Luluh Bahan
-    Sn = float(request.POST['Sn']) 
+    P = float(request.POST['P']) #Daya
+    n = float(request.POST['n']) #Kecepatan Putar
     Ft = float(request.POST['Ft']) #Gaya Tangensial Pada Elemen
     Fr = float(request.POST['Fr']) #Gaya Radial Pada Elemen
     AB = float(request.POST['AB']) #Jarak Antara Bantalan A ke Elemen B
     BC = float(request.POST['BC']) #Jarak Antara Bantalan C ke Elemen B
+    
     #Bahan
-    bahan = (request.POST['material'])
-    bahan_split = bahan.split(", ")
-    Su = float(bahan_split[0])
-    Sy = float(bahan_split[1])
+    id = request.POST['Material']
+    material = Materials.objects.get(id=id)
+    Su = material.tegangan_tarik
+    Sy = material.tegangan_luluh
 
     # Menghitung Gaya Reaksi Tumpuan
     Az = (Ft * BC) / (AB + BC)
     Cz = (Ft * AB) / (AB + BC)
     Ay = (Fr * BC) / (AB + BC)
     Cy = (Fr * AB) / (AB + BC)
+
+    # Menghitung Torsi
+    T = (P * 1000 / (math.pi * n / 30) ) * 1000
 
     # Menghitung Gaya Geser Gabungan
     Vc_horizontal = -Az + Ft
@@ -45,6 +50,29 @@ def output(request):
     M_horizontal = Az * AB
     M_vertikal = Ay * AB
     M_total = math.sqrt((M_horizontal)**2 + (M_vertikal)**2)
+
+    # Tegangan / Kekuatan Lelah Aktual Sn.Cs.Cr
+    # Data Gambar 5.11 Kekuatan lelah vs Tegangan tarik
+    data = np.array([(350, 138), (375, 143), (400, 150), (425, 163), (450, 172),
+                 (475, 181), (500, 190), (525, 201), (550, 211), (575, 222),
+                 (600, 227), (625, 237), (650, 247), (675, 254), (700, 263),
+                 (725, 273), (750, 277), (775, 287), (800, 295), (825, 301),
+                 (850, 311), (875, 319), (900, 325), (925, 335), (950, 343),
+                 (975, 348), (1000, 352), (1025, 358), (1050, 365), (1075, 375),
+                 (1100, 381), (1125, 388), (1150, 398), (1175, 402), (1200, 408), 
+                 (1225, 413), (1250, 421), (1275, 425), (1300, 428), (1350, 437), 
+                 (1400, 447), (1500, 457)])
+    # Didapat x dan y
+    x = data[:, 0]
+    y = data[:, 1]
+    # Fit a polynomial of degree 15 to the data
+    koefs = np.polyfit(x, y, 15)
+    # Buat fungsi dari the koefisien
+    poly_func = np.poly1d(koefs)
+    # Calculate the corresponding value of y using the polynomial function
+    S = poly_func(Su)
+    # Didapat kekuatan lelah aktual
+    Sn = S * 0.75 * 0.81
 
     # Kt lokasi bantalan 2.0; roda gigi 2.5
     # Momen Lentur pada titik A = 0
@@ -57,7 +85,10 @@ def output(request):
         "result_a": result_a,
         "result_b": result_b,
         "result_c": result_c,
-        "Su": Su
+        "Su": Su,
+        "Sy": Sy,
+        "T": T,
+        "Sn": Sn
     })
 
 def about(request):
